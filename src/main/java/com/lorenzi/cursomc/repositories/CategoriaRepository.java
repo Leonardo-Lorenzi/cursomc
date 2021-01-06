0334f54d8ca63274d088d3835ee89b4127bb0e2a
Leonardo Lorenzi <leonardo.lorenzi147@gmail.com>
2021-01-06 21:35:57 -0200

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/lorenzi/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.lorenzi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lorenzi.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	
+}
