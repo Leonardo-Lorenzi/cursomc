c5e777e355a67d996d1ebb9e7fddb4ea343bc267
Leonardo Lorenzi <leonardo.lorenzi147@gmail.com>
2021-01-20 23:09:56 -0200

    Endpoint pedidos{id}

---
+++ src/main/java/com/lorenzi/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.lorenzi.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lorenzi.cursomc.domain.Pedido;
+import com.lorenzi.cursomc.repositories.PedidoRepository;
+import com.lorenzi.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
